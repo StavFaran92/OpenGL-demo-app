fde1d3d5e23a6e1852ba07df96eb07776a74a372
stav faran <41070216+StavFaran92@users.noreply.github.com>
2023-07-12 19:29:07 +0300

    fixed a memory bug i caused without noticing

--- Engine/src/Scene.cpp
+++ Engine/src/Scene.cpp
@@ -153,7 +153,7 @@ void Scene::update(float deltaTime)
 
 		for (physx::PxRigidActor* actor : actors)
 		{
-			entity_id id = static_cast<entity_id>(reinterpret_cast<uintptr_t>(actor->userData));
+			entity_id id = *(entity_id*)actor->userData;
 			Entity e{ entt::entity(id), this };
 			auto& transform = e.getComponent<Transformation>();
 
@@ -469,7 +469,8 @@ void Scene::startSimulation()
 		assert(body);
 
 		m_PhysicsScene->addActor(*body);
-		body->userData = reinterpret_cast<void*>(static_cast<uintptr_t>(e.handlerID()));
+		entity_id* id = new entity_id(e.handlerID());
+		body->userData = (void*)id;
 	}
 
 	for (auto&& [entity, collider] : m_registry.view<CollisionSphereComponent>().each())
@@ -502,7 +503,8 @@ void Scene::startSimulation()
 		assert(body);
 
 		m_PhysicsScene->addActor(*body);
-		body->userData = reinterpret_cast<void*>(static_cast<uintptr_t>(e.handlerID()));
+		entity_id* id = new entity_id(e.handlerID());
+		body->userData = (void*)id;
 	}
 
 	for (auto&& [entity, collider] : m_registry.view<CollisionConvexMeshComponent>().each())
@@ -536,7 +538,8 @@ void Scene::startSimulation()
 		assert(body);
 
 		m_PhysicsScene->addActor(*body);
-		body->userData = reinterpret_cast<void*>(static_cast<uintptr_t>(e.handlerID()));
+		entity_id* id = new entity_id(e.handlerID());
+		body->userData = (void*)id;
 		//rb.simulatedBody = body;
 	}
 
