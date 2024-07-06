1c00555e98b81464bf2ffc15431c3fb9b8b5907f
stav faran <41070216+StavFaran92@users.noreply.github.com>
2024-07-06 16:10:51 +0300

    fix event system unregister issue

--- Engine/src/EventSystem.cpp
+++ Engine/src/EventSystem.cpp
@@ -1,15 +1,33 @@
 #include "EventSystem.h"
 
-void EventSystem::addEventListener(SDL_EventType eventType, const std::function<void(SDL_Event e)>& callback)
+uint64_t EventSystem::addEventListener(SDL_EventType eventType, const std::function<void(SDL_Event e)>& callback)
 {
 	if (m_listeners.find(eventType) == m_listeners.end())
-		m_listeners[eventType] = std::vector<std::function<void(SDL_Event e)>>();
-	m_listeners[eventType].emplace_back(callback);
+	{
+		m_listeners[eventType] = {};
+	}
+
+	uint64_t listenterID = ++s_listeners;
+
+	m_listeners[eventType].insert(listenterID);
+
+	m_callbacks[listenterID] = { true, callback };
+
+	return listenterID;
 }
 
-void EventSystem::removeEventListener(std::function<void(SDL_Event e)> callback)
+void EventSystem::addEventListener(SDL_EventType eventType, const std::function<void(SDL_Event e)>& callback, std::shared_ptr<Handler>& handler)
 {
+	addEventListener(eventType, callback);
+}
 
+void EventSystem::removeEventListener(uint64_t listenerID)
+{
+	auto iter = m_callbacks.find(listenerID);
+	if (iter == m_callbacks.end()) 
+		return;
+
+	iter->second.isValid = false;
 }
 
 void EventSystem::pushEvent(SDL_Event e)
@@ -19,8 +37,18 @@ void EventSystem::pushEvent(SDL_Event e)
 
 void EventSystem::dispatch(SDL_Event e)
 {
-	for (const auto& listener : m_listeners[static_cast<SDL_EventType>(e.type)])
+	auto& listeners = m_listeners[static_cast<SDL_EventType>(e.type)];
+	auto iter = listeners.begin();
+	while (iter != listeners.end())
 	{
-		listener(e);
+		auto& cb = m_callbacks[*iter];
+		if (!cb.isValid)
+		{
+			iter = listeners.erase(iter);
+			continue;
+		}
+
+		cb.func(e);
+		iter++;
 	}
 }
--- Engine/src/EventSystem.h
+++ Engine/src/EventSystem.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
+#include <memory>
 #include <functional>
 
 #include "SDL2/SDL_events.h"
@@ -10,12 +12,37 @@
 class EngineAPI EventSystem
 {
 public:
-	void addEventListener(SDL_EventType eventType, const std::function<void(SDL_Event e)>& callback);
-	void removeEventListener(std::function<void(SDL_Event e)> callback);
+	class Handler
+	{
+		Handler(EventSystem* eSystem, uint64_t listenerID) : m_eventSystem(eSystem), m_listenerID(listenerID) {}
+
+		~Handler()
+		{
+			m_eventSystem->removeEventListener(m_listenerID);
+		}
+
+	private:
+		EventSystem* m_eventSystem;
+		const uint64_t m_listenerID;
+	};
+
+	struct Callback
+	{
+		bool isValid = true;
+		std::function<void(SDL_Event e)> func;
+	};
+
+	uint64_t addEventListener(SDL_EventType eventType, const std::function<void(SDL_Event e)>& callback);
+	void addEventListener(SDL_EventType eventType, const std::function<void(SDL_Event e)>& callback, std::shared_ptr<Handler>& handler);
+	void removeEventListener(uint64_t listenerID);
 	void pushEvent(SDL_Event e);
 private:
 	friend class Engine;
 	void dispatch(SDL_Event e);
 private:
-	std::unordered_map<SDL_EventType, std::vector<std::function<void(SDL_Event e)>>> m_listeners;
+	std::unordered_map<SDL_EventType, std::unordered_set<uint64_t>> m_listeners;
+
+	std::unordered_map < uint64_t, Callback > m_callbacks;
+
+	inline static uint64_t s_listeners = 0;
 };
