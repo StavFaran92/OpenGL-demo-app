2e1eebb4f9841d35f8aac80f6d359833de1f536a
stav faran <41070216+StavFaran92@users.noreply.github.com>
2024-09-17 10:29:02 +0300

    final touches

--- Engine/src/MeshExporter.cpp
+++ Engine/src/MeshExporter.cpp
@@ -9,7 +9,7 @@ std::string MeshExporter::exportMesh(Resource<Mesh> mesh, const aiScene* scene)
 	auto& projectDir = Engine::get()->getProjectDirectory();
 	Assimp::Exporter exporter;
 	const std::string savedFilePath = projectDir + "/" + mesh.getUID() + ".dae";
-	exporter.Export(scene, "collada", savedFilePath/*, aiProcess_Triangulate*/);
+	exporter.Export(scene, "collada", savedFilePath);
 	Engine::get()->getContext()->getProjectAssetRegistry()->addMesh(mesh.getUID());
 	return savedFilePath;
 }
